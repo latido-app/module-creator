2090c615599f978837164ec49d41f5e65ff8e8bf
Joel Matthys <joel@matthysmusic.com>
2015-09-09 00:22:34 -0500

    initial commit

---
+++ create_module.py
@@ -0,0 +1,64 @@
+from music21 import *
+import xml.dom.minidom as xml
+import glob
+
+libnameinput = raw_input('full lib name? ')
+keyinput = raw_input('module key? (8 chars) ')
+
+if False:
+    for fname in sorted(glob.glob('*.xml')):
+        try:
+            test = converter.parse(fname)
+            sopr = test.parts[0]
+            sopr.write('lily.png', 'image/'+fname[:-4])
+            print 'succeeded: '+fname
+        except:
+            print "failed: "+fname
+
+def calculate_countin (mfile):
+    timesig = mfile.flat.getElementsByClass(meter.TimeSignature)
+    beats = 0
+    if 1 == len(timesig):
+        beats = timesig[0].numerator
+        countin = beats
+    else:
+        beats = timesig[1].numerator
+        countin = beats - timesig[0].numerator
+    if beats > 3 and beats % 3 == 0:
+        beats = beats / 3.0
+        countin = countin / 3.0
+    if countin <= 1:
+        countin = countin + beats
+    return countin
+
+doc = xml.Document()
+latido = doc.createElement("latido")
+xmlname = doc.createElement("name")
+modulekey = doc.createElement("modulekey")
+xmlimage = doc.createElement("imageextension")
+xmlmidi = doc.createElement("midiextension")
+xmlprogress = doc.createElement("progress")
+doc.appendChild(latido)
+latido.appendChild(xmlname)
+latido.appendChild(modulekey)
+latido.appendChild(xmlimage)
+latido.appendChild(xmlmidi)
+latido.appendChild(xmlprogress)
+xmlname.appendChild(doc.createTextNode(libnameinput))
+modulekey.appendChild(doc.createTextNode(keyinput))
+xmlimage.appendChild(doc.createTextNode("png"))
+xmlmidi.appendChild(doc.createTextNode("mid"))
+for midifile in sorted(glob.glob('midi/*.mid')):
+    xmlexercise = doc.createElement('exercise')
+    xmlexercise.setAttribute('name',midifile[5:-4])
+    xmlexercise.setAttribute('tempo','120')
+    temp = converter.parse(midifile)
+    countin = calculate_countin(temp)
+    xmlexercise.setAttribute('countin',str(countin))
+    xmlprogress.appendChild(xmlexercise)
+fn = keyinput + '.xml'
+doc.writexml( open(fn, 'w'),
+               indent="  ",
+               addindent="  ",
+               newl='\n')
+doc.unlink()
